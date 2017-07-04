0c0c7676ea3a52fafb201fd7e58fbd590c5fbf4c
Nhlamulomax <nhlamulomax@gmail.com>
2017-07-04 19:07:49 +0200

    Last test,

--- Task1.py
+++ Task1.py
@@ -1,3 +1,4 @@
 str = 'Hellows MDM, This will be task 1'
 print("lets see if you updating")
+print("")
 print(str)
--- Task2.py
+++ Task2.py
@@ -1,3 +1,4 @@
 str = 'Hellows MDM, This will be task 2'
 print("Are you updating to GitHub?")
+print("")
 print(str)
