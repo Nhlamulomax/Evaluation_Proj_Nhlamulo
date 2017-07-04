c5cdc65679b4c28aa45cc343e82906f017d80aee
Nhlamulomax <nhlamulomax@gmail.com>
2017-07-04 15:41:27 +0200

    This is the main branch for the project
    This is the evaluation project

---
+++ Task1.py
@@ -0,0 +1,6 @@
+str = 'Hellows MDM, This will be task 1'
+#This is a comment on task1
+
+
+
+print(str)
---
+++ Task2.py
@@ -0,0 +1,3 @@
+str = 'Hellows MDM, This will be task 2'
+#This is a comment on task2
+print(str)
