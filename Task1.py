a33af986d54528b7e0cc73aead97440205b8e711
Nhlamulomax <nhlamulomax@gmail.com>
2017-07-05 13:53:30 +0200

    Updates

--- Task1.py
+++ Task1.py
@@ -1,10 +1,28 @@
-from flask import Flask
+#import requests
+#from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+#r = requests.get("https://slashdot.org")
+#r.content
+#soup = BeautifulSoup(r.content)
 
-@app.route('/')
-def hello():
-    return "hello MDM, I am testing some libraries"
+#print soup.prettify()
+########################################################
 
-if __name__ == "__main__":
-    app.run()
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://slashdot.org"
+req = requests.get(url)
+
+soup = BeautifulSoup(req.content)
+links = soup.find_all("a")
+
+#Print all the links
+#for link in links:
+       # print "<a href='%s'>%s</a>" % (link.get("href"), link.text)
+
+#g_headlines = soup.find_all("div", {"class": "infor"})
+g_headlines = soup.find_all("a", {"href": "//hardware.slashdot.org/story/17/07/05/023222/call-for-a-ban-on-child-sex-robots"})
+
+for h in g_headlines:
+    print h.text
--- Task2.py
+++ Task2.py
@@ -1,4 +1,12 @@
-str = 'Hellows MDM, This will be task 2'
-print("Are you updating to GitHub?")
-print("   ")
-print(str)
+
+
+
+
+
+
+
+
+
+
+
+
