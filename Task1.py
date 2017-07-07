a2bdfaa0a72714ac72883ed2d898a264e158c67d
Nhlamulomax <nhlamulomax@gmail.com>
2017-07-07 19:39:44 +0200

    Update

--- Task1.py
+++ Task1.py
@@ -3,45 +3,41 @@ from bs4 import BeautifulSoup as soup
 
 my_url = "https://slashdot.org/"
 
-#opening up the connection, grabbing the page
+# opening up the connection, grabbing the page
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
 
-#html parsing
+# html parsing
 page_soup = soup(page_html, "html.parser")
 
-#Grabs each field
-containers = page_soup.findAll("article",{"class":"fhitem fhitem-story article usermode thumbs grid_24"})
+# Grabs each product
+containers = page_soup.findAll("article", {"class": "fhitem fhitem-story article usermode thumbs grid_24"})
 
 ##Get the name of the link, which is the headline
-#container.header.h2.span.a.text
+# container.header.h2.span.a.text
 
-#Also using CSV file to display my news headlines
-#filename = "product.csv"
-#f = open(filename, "w")
+# filename = "product.csv"
+# f = open(filename, "w")
 
-#headers = "Task 1: News headlines\n"
+# headers = "band, product_name, shipping\n"
 
-#f.write(headers)
+# f.write(header)
 
+print("[")
 for container in containers:
+    print("{")
+    headline = container.header.h2.span.a.text
 
-	headline = container.header.h2.span.a.text
+    # author_container = container.findAll("a", {"href":"https://twitter.com/BeauHD"})
+    # author = author_container[0].text
 
-	author_container = container.findAll("a", {"href":"https://twitter.com/BeauHD"})
-	author = author_container[0].text
+    # date_container = container.findAll("time", {"datetime":"on Thursday July 06, 2017 @06:00AM"})
+    # datee = date_container[0].text
+    # dyt = datetime.datetime(date_container, "%Y-%m-%dT%H:%M:%S+00:00")
 
-	#date_container = container.findAll("time", {"datetime":"on Thursday July 06, 2017 @06:00AM"})
-	#datee = date_container[0].text
-
-	print("Headline: " + headline)
-	print("Author name: " + author)
-	#print("Date: " + datee)
-
-#datee = 10
-
-	#f.write(headline + "," + author + "," + datee + "\n")
-#f.close()
-
-
+    print("Headline: " + headline)
+    print("Author: " + "author")
+    print("Date: " + "Put timestamp here")
+    print("},")
+print("]")
