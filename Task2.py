64aff72d3a2c8a72602954557adda711065ae8c6
Nhlamulomax <nhlamulomax@gmail.com>
2017-07-10 08:25:45 +0200

    Updates
    Task two under construction

--- Task2.py
+++ Task2.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 #connect and read the site with url provided
-my_url = "http://www.imdb.com/find?ref_=nv_sr_fn&q=Movies&s=tt"
+my_url = "http://www.imdb.com/find?q=Movies&s=tt&ttype=ft&ref_=fn_ft"
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
@@ -12,29 +12,25 @@ uClient.close()
 #parsing html
 page_soup = soup(page_html, "html.parser")
 
-#Grabs each movie's title
+#Grabs all movie's title
 containers = page_soup.findAll("td", {"class":"result_text"})
-
+tit = containers[0]
+title = tit.text
+print(title)
 #input search string
 search_string = input("Enter a searsch string: ")
-
-#loop for frst 10 titles
-i = 0
-while i <= 9:
-
-    title = str(containers[i])
-
-    if title == search_string:
+#SearchMe = "The apple is red and the berry is blue!"
+#print(SearchMe.find("is"))
+print(title.find(search_string))
+num = title.find(search_string)  #number of searched string
+if num > 0:
+    # loop for frst 10 titles
+    i = 0
+    while i <= 9:
         # print the Title and date of each tittle
         print("Title: " + title)
         print("Date: " + "Date here")
         print("")
         i = i + 1
-
-
-
-
-
-
 
 
