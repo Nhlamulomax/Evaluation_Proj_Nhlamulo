72d6c2ed26a5faf6193c611a7ae36a689d189216
Nhlamulomax <nhlamulomax@gmail.com>
2017-07-12 15:43:41 +0200

    TASK1 is done
    Task1 is done

--- Task1.py
+++ Task1.py
@@ -18,9 +18,13 @@ try:
     password = br.find_element_by_id('upasswrd')
     password.send_keys('nhlamulo@Slashdot')
     password.submit()
+    br.close()
 except:
-    print("")
+    pass
 finally:
+    #writing to excel:BONUS
+
+
     # Scraping code
     my_url = "https://slashdot.org/"
 
@@ -36,7 +40,7 @@ finally:
     containers = page_soup.findAll("article", {"class": "fhitem fhitem-story article usermode thumbs grid_24"})
 
     # Input timestamp
-    input_tymstamp = input("                                              Enter the timestamp: ")
+    input_date = input("Enter the date -> [yyyy-mm-dd]:")
 
     print("")
     print("                                          NEWS HEADLINES:\n                                 ")
@@ -46,10 +50,10 @@ finally:
 
         print("{")
 
-        # Headline
+        # Scrap the Headline
         headline = container.header.h2.span.a.text
 
-        # Author
+        # Scrap the Author
         author_container = container.header.div.findAll("span", {"class": "story-byline"})
         # author name in new line
         author_name = author_container[0].text
@@ -60,56 +64,41 @@ finally:
         # removes all chars aftr space char
         author = author_frst.split(' ')[0]
 
-        # Timestamp
-        date_container = container.header.div.findAll("span", {"class": "story-byline"})
-        # author name in new line
-        date_name = date_container[0].text
-        # Removes new lines in a string
-        date_rem_lines = re.sub(r"\s+", " ", date_name)
-        # remove frst 10 chars
-        date_frst = date_rem_lines[11:]  # msmash on Friday July 07, 2017 @09:47PM from the tussle-continues dept.
-        # Remove author name
-        rem_author = date_frst.lstrip(author)  # on Friday July 07, 2017 @09:47PM from the tussle-continues dept.
-        # Remove 'on ' in date_date
-        rem_ON = rem_author.lstrip('on ')  # Friday July 07, 2017 @09:47PM from the tussle-continues dept.
-        # Remove everything and liv the date
-        date_date = rem_ON.split(' from')[0]  # Friday July 07, 2017 @09:47PM
-        # Day
-        my_day = date_date.split(' ')[0]  # Friday
-        # remove day
-        date_time = date_date.lstrip(my_day)
-        # Trim date_time
-        trim_date_time = date_time.strip()  # July 07, 2017 @05:21PM
-        # my_month
-        my_month = trim_date_time.split(' ')[0]  # July
-        # Remove month
-        rem_month = trim_date_time.lstrip(my_month)
-        # Trim
-        trim_rem_month = rem_month.strip()
-        # my_daynum
-        my_daycom = trim_rem_month.split(' ')[0]  # 07,
-        my_daynum = my_daycom.split(',')[0]  # 07
-        # Remove month and day
-        rem_month_day = trim_date_time.lstrip(my_month + " " + my_daycom)  # 2017 @05:21PM
-        my_year = (rem_month_day.split(' ')[0]).strip()
-        # Month format
-        month_format = my_month[:3]
-        # Change month name to month num
-        month_name = month_format
-        month_number = str(strptime(month_name, '%b').tm_mon)
-        test = datetime.datetime.now().date()  # Today's date
-        # TimeStamp
-        d8tym = date(int(my_year), int(month_number), int(my_daynum))
-        timestamp2 = time.mktime(d8tym.timetuple())
-        # Today's timestamp
-        tday = date.today()
-        tdays_timestamp = time.mktime(tday.timetuple())  # 1499551200.0
+        #Scrap the Date
+        def fix_date(my_date):
+            #Get the month name
+            def monthNum(x):
+                return {
+                    'january': 1,
+                    'february': 2,
+                    'march': 3,
+                    'april': 4,
+                    'may': 5,
+                    'june': 6,
+                    'july': 7,
+                    'august': 8,
+                    'september': 9,
+                    'october': 10,
+                    'november': 11,
+                    'december': 12
+                }.get(x, None)
 
-        # Output the headlines, author and date of ne
-        if float(input_tymstamp) <= float(tdays_timestamp):  # 1499464800.0
+            date_format = (my_date[4] + str(monthNum(my_date[2].lower())) + my_date[3]).replace(',', '')
+            fixed_date = datetime.datetime.strptime(date_format, "%Y%m%d").date()
+
+            return fixed_date
+
+        article_date = fix_date(container.parent.find('time').text.split(' ')).strftime('%Y-%m-%d')      #2017-07-12
+
+        timeStamp = int(time.mktime(datetime.datetime.strptime(article_date, "%Y-%m-%d").timetuple()))
+
+        if input_date < article_date:
             print("  Headline: " + headline)
             print("  Author: " + author)
-            print("  Date: " + str(timestamp2))
+            #print("  Date: " + article_date)
+            print("  Date: " + str(timeStamp))
+        else:
+            print("Nothing to be displayed, SORRY!!!")
 
         print("},")
     print("]")
